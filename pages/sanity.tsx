684bc55b8dd1a03d8d7c5a52b0b7294a86a341b7
DoRim Jang <dorimjang@DoRims-iMac.local>
2023-01-20 20:46:32 -0600

    update

--- pages/sanity.tsx
+++ pages/sanity.tsx
@@ -3,5 +3,7 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
  projectId: 'port',
  dataset: 'portdata',
+ apiVersion: '2021-08-31',
+ token: 'sanity-auth-token',
  useCdn: true // `false` if you want to ensure fresh data
 });
