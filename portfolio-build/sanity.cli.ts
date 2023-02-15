f7e57de6ffa500292bc3b60294928adb65eb86d4
Blck Strt <97315523+BlockStrt@users.noreply.github.com>
2023-02-14 20:05:34 -0600

    update

--- portfolio-build/sanity.cli.ts
+++ portfolio-build/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '7fnjll7q',
-    dataset: 'production'
+    dataset: 'production',
   }
 })
--- sanity.ts
+++ sanity.ts
@@ -1,17 +1,14 @@
-import { createClient, ClientConfig, SanityClient } from 'next-sanity'
+import { createClient } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
-interface CustomClientConfig extends ClientConfig {
-   exclude?: RegExp[]
- }
- type CustomSanityClientLike = SanityClient & CustomClientConfig
 
- export const config:  CustomSanityClientLike = {
+
+ export const config = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "pv8y60vp",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
     apiVersion: "2022-11-16" ,// https://www.sanity.io/docs/api-versioning
     useCdn: process.env.Node_ENV === 'production',
-    exclude: [/^\/sanity\//], // Exclude all paths that start with "sanity/"
+    exclude: ["node_modules", "./sanity"], // Exclude all paths that start with "sanity/"
  }
 
   export const sanityClient = createClient(config);
