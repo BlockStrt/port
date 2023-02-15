ea6d40b3563560c4c6c2fe0f8327605319ab903a
Blck Strt <97315523+BlockStrt@users.noreply.github.com>
2023-02-14 19:27:40 -0600

    update

--- sanity.ts
+++ sanity.ts
@@ -1,13 +1,16 @@
-import { createClient } from 'next-sanity'
+import { createClient, ClientConfig } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
+interface CustomClientConfig extends ClientConfig {
+   exclude?: RegExp[]
+ }
 
-
- export const config = {
+ export const config:  CustomClientConfig = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "pv8y60vp",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
     apiVersion: "2022-11-16" ,// https://www.sanity.io/docs/api-versioning
-    useCdn: process.env.Node_ENV === 'production'
+    useCdn: process.env.Node_ENV === 'production',
+    exclude: [/^\/sanity\//], // Exclude all paths that start with "sanity/"
  }
 
   export const sanityClient = createClient(config);
